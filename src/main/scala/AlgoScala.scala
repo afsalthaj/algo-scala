d855c5d6c2d3ab0b6647385ceee0d04c072fe8c4
Afsal Thaj <afsal.taj06@gmail.com>
2017-07-15 12:15:08 +1000

    adding more comments into karatsuba

--- src/main/scala/AlgoScala.scala
+++ src/main/scala/AlgoScala.scala
@@ -1,11 +1,20 @@
 import java.math.BigInteger
 
-
 /**
   * Created by afsalthaj on 7/10/17.
   */
 object AlgoScala {
-  def myAwesomeMultiplication(x: String, y: String): BigInt = {
+  /**
+    * Karatsuba which has a O(n log n)
+    * Formula is derived from the concept
+    * ab * ef (Ex: 1234 * 5678 ===> a = 12, b = 34 etc)
+    * (10^^(n/2)*a + b) * (10 ^^(n/2)*c + d) ===> to derive
+    * (10 ^^ n)*ac + (ad + bc) * (10 ^^ (n/2) + bd
+    * And the numbers can be recursively multiplied to have
+    * a big O of (n log n) instead of n^^2 etc in usual
+    * multiplication algorithms.
+    */
+  def karatSuba(x: String, y: String): BigInt = {
     def go(xx: String, yy: String): BigInt = {
       if (yy.length <=2 || xx.length <= 2) {
         BigInt(xx) * BigInt(yy)
@@ -33,7 +42,9 @@ object AlgoScala {
     go(x, y)
   }
 
+  // To be avoided: use of string? Bit Opertions?
+  // Use of BigInt seems to be a bit hacky
   def main(args: Array[String]): Unit = {
-    val result = myAwesomeMultiplication("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627")
+    val result = karatSuba("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627")
     println (result)
   }}
