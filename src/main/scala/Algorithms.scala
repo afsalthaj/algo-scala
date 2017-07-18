2a3457191aae9807e6ca1819d1b82187c4009715
Afsal Thaj <afsal.taj06@gmail.com>
2017-07-18 21:32:52 +1000

    added binary search

--- src/main/scala/Algorithms.scala
+++ src/main/scala/Algorithms.scala
@@ -1,7 +1,39 @@
 /**
   * Created by afsalthaj on 7/16/17.
   */
-object MergeSort {
+object Algorithms {
+  /**
+    * Karatsuba multiplication with lesser runtime (big O) than
+    * usual multiplication algorithms
+    */
+  def karatSuba(x: String, y: String): BigInt = {
+    def go(xx: String, yy: String): BigInt = {
+      if (yy.length <= 2 || xx.length <= 2) {
+        BigInt(xx) * BigInt(yy)
+      }
+      else {
+        val xString = xx.toString.toList
+        val yString = yy.toString.toList
+        val halfN: Int = Math.max(xString.size, yString.size) / 2
+        val xSplitt = xString.size - halfN
+        val ySplitt = yString.size - halfN
+        val a = BigInt(xString.take(xSplitt).mkString)
+        val b = BigInt(xString.drop(xSplitt).mkString)
+        val c = BigInt(yString.take(ySplitt).mkString)
+        val d = BigInt(yString.drop(ySplitt).mkString)
+        val ac = go(a.toString, c.toString)
+        val bd = go(b.toString, d.toString)
+        val abcd = go((a + b).toString, (c + d).toString)
+        val adplusbc = abcd - (ac + bd)
+        val toBeFilled = BigInt(s"10${List.fill(halfN * 2)(0).mkString}")
+        val secondToBeFilled = BigInt(s"10${List.fill(halfN)(0).mkString}")
+        (toBeFilled * ac) + (secondToBeFilled * adplusbc) + bd
+      }
+    }
+
+    go(x, y)
+  }
+
   /**
     * For a merge sort, the number of times the recursion can happen, given an input
     * array is equal log n (2) : The number of times n should be divided by 2
@@ -40,5 +72,25 @@ object MergeSort {
           inner(continueSorting1, continueSorting2, Nil, inversions1 + inversions2)
       }
     }
+  }
+
+  /**
+    * According to master theorem
+    * a; number of recursions = 2
+    * b; shrinkage factor = 2
+    * d = 0 (as there are zero steps outside recursion)
+    * T(n) <= n log n
+    */
+  def binarySearch(xx: List[Int], y: Int): Boolean = {
+    val firstHalf = xx.take(xx.size / 2)
+    val secondHalf = xx.drop(firstHalf.size)
+    if (secondHalf.isEmpty)
+      false
+    else if (y == secondHalf.head)
+      true
+    else if (y > secondHalf.head)
+      binarySearch(xx.tail, y)
+    else
+      binarySearch(firstHalf, y)
   }
 }
